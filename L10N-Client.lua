ba43c6e6211c6047fa3f65044c2a6f682d89519d
Callum Dawson <CallumDawson@users.noreply.github.com>
2019-05-19 16:45:33 +0100

    Create L10N-Client.lua

---
+++ L10N-Client.lua
@@ -0,0 +1,134 @@
+L10N = {
+	ACCOUNTS_TITLE = {
+		en_US = "Account Login",
+		nl = "Account Login"
+	},
+	ACCOUNTS_REGISTER_TITLE = {
+		en_US = "Account Registration",
+		nl = "Account Registratie"
+	},
+	ACCOUNTS_USERNAME = {
+		en_US = "Username:",
+		nl = "Gebruikersnaam"
+	},
+	ACCOUNTS_PASSWORD = {
+		en_US = "Password:",
+		nl = "Wachtwoord"
+	},
+	ACCOUNTS_PASSWORD_CONFIRM = {
+		en_US = "Confirm:",
+		nl = "Bevestig:"
+	},
+	ACCOUNTS_EMAIL = {
+		en_US = "Email:",
+		nl = "Email:"
+	},
+	ACCOUNTS_REMEMBER = {
+		en_US = "Remember Password",
+		nl = "Wachtwoord Onthouden"
+	},
+	ACCOUNTS_LOGIN = {
+		en_US = "Login",
+		nl = "Inloggen"
+	},
+	ACCOUNTS_REGISTER = {
+		en_US = "Register",
+		nl = "Registreer"
+	},
+	ACCOUNTS_CANCEL = {
+		en_US = "Cancel",
+		nl = "Annuleren"
+	},
+	ACCOUNTS_RECOVERY = {
+		en_US = "Account Recovery",
+		nl = "Account Herstellen"
+	},
+	ACCOUNTS_PLAYERS = {
+		en_US = "Players: %s",
+		nl = "Spelers: %s"
+	},
+	ACCOUNTS_ERROR_FIELDS = {
+		en_US = "You did not complete all of the fields!",
+		nl = "Je hebt niet alle velden voltooid!"
+	},
+	ACCOUNTS_ERROR_PASSWORD_MATCH = {
+		en_US = "Your passwords do not match!",
+		nl = "Je wachtwoorden komen niet overeen!"
+	},
+	ACCOUNTS_ERROR_EMAIL = {
+		en_US = "You specified an invalid email address!",
+		nl = "Je hebt een ongeldig emailadres opgegeven!"
+	},
+	ACCOUNTS_ERROR_CREDENTIALS = {
+		en_US = "Invalid username/password!",
+		nl = "Ongeldige gebruikersnaam/wachtwoord!"
+	},
+	ACCOUNTS_ERROR_EXISTS = {
+		en_US = "Account already exists!",
+		nl = "Account bestaat al!"
+	},
+	ACCOUNTS_ERROR_UNKNOWN = {
+		en_US = "An unknown error occurred!",
+		nl = "Er is een onbekende fout opgetreden!"
+	},
+	SCOREBOARD_JOB = {
+		en_US = "Job",
+		nl = "Baan"
+	},
+	SCOREBOARD_PLAYTIME = {
+		en_US = "Playtime",
+		nl = "Speeltijd"
+	},
+	JOB_INFORMATION = {
+		en_US = "Job Information",
+		nl = "Werk Informatie"
+	},
+	JOB_TITLE = {
+		en_US = "Job Title:",
+		nl = "Werk Titel:"
+	},
+	JOB_TEAM = {
+		en_US = "Team:",
+		nl = "Team:"
+	},
+	JOB_DESCRIPTION = {
+		en_US = "Description:",
+		nl = "Beschrijving:"
+	},
+	JOB_ACCEPT = {
+		en_US = "Accept Job",
+		nl = "Accepteer Baan"
+	},
+	SPAWNER_TITLE = {
+		en_US = "Vehicle Spawner",
+		nl = "Voertuig Spawner"
+	},
+	SPAWNER_VEHICLE = {
+		en_US = "Vehicle",
+		nl = "Voertuig"
+	},
+	SPAWNER_SPAWN = {
+		en_US = "Spawn",
+		nl = "Spawn"
+	},
+	SPAWNER_CLOSE = {
+		en_US = "Close",
+		nl = "Sluiten"
+	},
+	CHAT_LOGIN = {
+		en_US = "* You must login to use chat!",
+		nl = "* Je moet ingelogd zijn om de chat te kunnen gebruiken!"
+	},
+	KILL_ALREADY_DEAD = {
+		en_US = "* You cannot use this command because you are already dead.",
+		nl = "* Je kunt deze command niet gebruiken omdat je al dood bent."
+	},
+	KILL_IN_VEHICLE = {
+		en_US = "* You cannot use this command because you are in a vehicle.",
+		nl = "* Je kunt deze command niet gebruiken omdat je in een voertuig bent."
+	},
+	KILL_GROUND = {
+		en_US = "* You cannot use this command because you are not on the ground.",
+		nl = "* Je kunt deze command niet gebruiken omdat je niet op de grond staat."
+	}
+}
