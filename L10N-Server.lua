94722afbd8daef353648dc870278f44f5381bb33
Callum Dawson <CallumDawson@users.noreply.github.com>
2019-05-19 16:45:58 +0100

    Create L10N-Server.lua

---
+++ L10N-Server.lua
@@ -0,0 +1,94 @@
+L10N = {
+	PLAYER_JOIN = {
+		en_US = "* %s has joined the game from %s",
+		nl= "%s heeft het spel gejoined vanuit %s"
+	},
+	PLAYER_QUIT = {
+		en_US = "* %s has left the game [%s]",
+		nl = "* %s heeft het spel verlaten [%s]"
+	},
+	PLAYER_NICK_CHANGE = {
+		en_US = "* %s is now known as %s",
+		nl = "* %s is nu bekend als %s"
+	},
+	JOB_RESIGNED = {
+		en_US = "* You have resigned from your job (%s).",
+		nl = "* Je hebt ontslag genomen van je baan (%s)."
+	},
+	JOB_NOT_EMPLOYED = {
+		en_US = "* You cannot resign as you are not currently employed.",
+		nl = "* Je kunt geen ontslag nemen aangezien je momenteel geen werk hebt."
+	},
+	CHAT_REPEATING = {
+		en_US = "* Stop repeating yourself!",
+		nl = "Stop met jezelf te herhalen!"
+	},
+	CHAT_TOO_FAST = {
+		en_US = "* You are chatting too fast!",
+		nl = "* Je bent te snel aan het chatten!"
+	},
+	CHAT_LOGIN = {
+		en_US = "* You must login to use chat!",
+		nl = "* Je moet ingelogd zijn om de chat te kunnen gebruiken!"
+	},
+	PEAK_NEW = {
+		en_US = "* A new player peak has been reached! The new peak is %s players.",
+		nl = "* Een nieuwe spelers piek is bereikt! De nieuwe piek is %s spelers."
+	},
+	PEAK_CURRENT = {
+		en_US = "* The current player peak is %s players.",
+		nl = "* De huidige spelers piek is %s spelers."
+	},
+	PEAK_ERROR = {
+		en_US = "* There was an error loading the player peak.",
+		nl = "* Er is een fout opgetreden tijdens het laden van de spelers piek."
+	},
+	PLAYTIME_REWARD = {
+		en_US = "* You have been paid $%s as a reward for playing on the server for at least 50 minutes.",
+		nl = "* Je hebt %s betaald gekregen als een beloning voor het spelen op deze server voor minimaal 50 minuten."
+	},
+	PLAYTIME_MONTH = {
+		en_US = "month",
+		nl = "maand"
+	},
+	PLAYTIME_MONTHS = {
+		en_US = "months",
+		nl = "maanden"
+	},
+	PLAYTIME_DAY = {
+		en_US = "day",
+		nl = "dag"
+	},
+	PLAYTIME_DAYS = {
+		en_US = "days",
+		nl = "dagen"
+	},
+	PLAYTIME_HOUR = {
+		en_US = "hour",
+		nl = "uur"
+	},
+	PLAYTIME_HOURS = {
+		en_US = "hours",
+		nl = "uren"
+	},
+	PLAYTIME_MINUTE = {
+		en_US = "minute",
+		nl = "minuut"
+	},
+	PLAYTIME_MINUTES = {
+		en_US = "minutes",
+		nl = "minuten"
+	},
+	PLAYTIME_SECOND = {
+		en_US = "second",
+		nl = "seconde"
+	},
+	PLAYTIME_SECONDS = {
+		en_US = "seconds",
+		nl = "seconden"
+	},
+	FIREFIGHTER_VEHICLE_FIRE = {
+		en_US = "* A vehicle has exploded in %s, %s.",
+		nl = "* Er is een voertuig geëxplodeerd in %s, %s."
+	}
+}
