5da1023f983b6e54b84a180a182fd8c457c7af7d
Kevin Gross <kevin.gross@globant.com>
2019-06-10 15:16:40 -0300

    Add spanish translation

--- L10N-Client.lua
+++ L10N-Client.lua
@@ -1,134 +1,167 @@
 L10N = {
 	ACCOUNTS_TITLE = {
 		en_US = "Account Login",
+		es = "Login",
 		nl = "Account Login"
 	},
 	ACCOUNTS_REGISTER_TITLE = {
 		en_US = "Account Registration",
+		es = "Registro de Cuenta",
 		nl = "Account Registratie"
 	},
 	ACCOUNTS_USERNAME = {
 		en_US = "Username:",
+		es = "Usuario:",
 		nl = "Gebruikersnaam"
 	},
 	ACCOUNTS_PASSWORD = {
 		en_US = "Password:",
+		es = "Contraseña:",
 		nl = "Wachtwoord"
 	},
 	ACCOUNTS_PASSWORD_CONFIRM = {
 		en_US = "Confirm:",
+		es = "Confirmar:",
 		nl = "Bevestig:"
 	},
 	ACCOUNTS_EMAIL = {
 		en_US = "Email:",
+		es = "Correo:",
 		nl = "Email:"
 	},
 	ACCOUNTS_REMEMBER = {
 		en_US = "Remember Password",
+		es = "Recordar Contraseña",
 		nl = "Wachtwoord Onthouden"
 	},
 	ACCOUNTS_LOGIN = {
 		en_US = "Login",
+		es = "Login",
 		nl = "Inloggen"
 	},
 	ACCOUNTS_REGISTER = {
 		en_US = "Register",
+		es = "Registrar",
 		nl = "Registreer"
 	},
 	ACCOUNTS_CANCEL = {
 		en_US = "Cancel",
+		es = "Cancelar",
 		nl = "Annuleren"
 	},
 	ACCOUNTS_RECOVERY = {
 		en_US = "Account Recovery",
+		es = "Recuperación de Cuenta",
 		nl = "Account Herstellen"
 	},
 	ACCOUNTS_PLAYERS = {
 		en_US = "Players: %s",
+		es = "Jugadores: %s"
 		nl = "Spelers: %s"
 	},
 	ACCOUNTS_ERROR_FIELDS = {
 		en_US = "You did not complete all of the fields!",
+		es = "No completaste todos los campos!",
 		nl = "Je hebt niet alle velden voltooid!"
 	},
 	ACCOUNTS_ERROR_PASSWORD_MATCH = {
 		en_US = "Your passwords do not match!",
+		es = "Las contraseñas no coinciden",
 		nl = "Je wachtwoorden komen niet overeen!"
 	},
 	ACCOUNTS_ERROR_EMAIL = {
 		en_US = "You specified an invalid email address!",
+		es = "Tu correo es inválido!",
 		nl = "Je hebt een ongeldig emailadres opgegeven!"
 	},
 	ACCOUNTS_ERROR_CREDENTIALS = {
 		en_US = "Invalid username/password!",
+		es = "Nombre de Usuario/Contraseña inválido!",
 		nl = "Ongeldige gebruikersnaam/wachtwoord!"
 	},
 	ACCOUNTS_ERROR_EXISTS = {
 		en_US = "Account already exists!",
+		es = "La cuenta ya existe!",
 		nl = "Account bestaat al!"
 	},
 	ACCOUNTS_ERROR_UNKNOWN = {
 		en_US = "An unknown error occurred!",
+		es = "Un error desconocido ocurrio!",
 		nl = "Er is een onbekende fout opgetreden!"
 	},
 	SCOREBOARD_JOB = {
 		en_US = "Job",
+		es = "Empleo",
 		nl = "Baan"
 	},
 	SCOREBOARD_PLAYTIME = {
 		en_US = "Playtime",
+		es = "Tiempo Jugado",
 		nl = "Speeltijd"
 	},
 	JOB_INFORMATION = {
 		en_US = "Job Information",
+		es = "Información de Empleo",
 		nl = "Werk Informatie"
 	},
 	JOB_TITLE = {
 		en_US = "Job Title:",
+		es = "Titulo de Empleo",
 		nl = "Werk Titel:"
 	},
 	JOB_TEAM = {
 		en_US = "Team:",
+		es = "Equipo:",
 		nl = "Team:"
 	},
 	JOB_DESCRIPTION = {
 		en_US = "Description:",
+		es = "Descripción:",
 		nl = "Beschrijving:"
 	},
 	JOB_ACCEPT = {
 		en_US = "Accept Job",
+		es = "Aceptar Empleo",
 		nl = "Accepteer Baan"
 	},
 	SPAWNER_TITLE = {
 		en_US = "Vehicle Spawner",
+		es = "Spawneador de Vehículos",
 		nl = "Voertuig Spawner"
 	},
 	SPAWNER_VEHICLE = {
 		en_US = "Vehicle",
+		es = "Vehículo",
 		nl = "Voertuig"
 	},
 	SPAWNER_SPAWN = {
 		en_US = "Spawn",
+		es = "Spawnear",
 		nl = "Spawn"
 	},
 	SPAWNER_CLOSE = {
 		en_US = "Close",
+		es = "Cerrar",
 		nl = "Sluiten"
 	},
 	CHAT_LOGIN = {
 		en_US = "* You must login to use chat!",
+		es = "* Debes estar logueado para usar el chat!",
 		nl = "* Je moet ingelogd zijn om de chat te kunnen gebruiken!"
 	},
 	KILL_ALREADY_DEAD = {
 		en_US = "* You cannot use this command because you are already dead.",
+		es = "* No puedes usar este comando porque estas muerto.",
 		nl = "* Je kunt deze command niet gebruiken omdat je al dood bent."
 	},
 	KILL_IN_VEHICLE = {
 		en_US = "* You cannot use this command because you are in a vehicle.",
+		es = "* No puedes usar este comando porque estas en un vehículo."
 		nl = "* Je kunt deze command niet gebruiken omdat je in een voertuig bent."
 	},
 	KILL_GROUND = {
 		en_US = "* You cannot use this command because you are not on the ground.",
+		es = "* No podes usar este comando porque no estas en el suelo.",
 		nl = "* Je kunt deze command niet gebruiken omdat je niet op de grond staat."
 	}
 }
--- L10N-Server.lua
+++ L10N-Server.lua
@@ -1,57 +1,71 @@
 L10N = {
 	PLAYER_JOIN = {
 		en_US = "* %s has joined the game from %s",
+		es = "* %s se ha unido al juego desde %s",
 		nl= "%s heeft het spel gejoined vanuit %s"
 	},
 	PLAYER_QUIT = {
 		en_US = "* %s has left the game [%s]",
+		es = "* %s ha abandonado el juego [%s]",
 		nl = "* %s heeft het spel verlaten [%s]"
 	},
 	PLAYER_NICK_CHANGE = {
 		en_US = "* %s is now known as %s",
+		es = "* %s ahora es conocido como %s",
 		nl = "* %s is nu bekend als %s"
 	},
 	JOB_RESIGNED = {
 		en_US = "* You have resigned from your job (%s).",
+		es = "* Has abandonado tu empleo (%s).",
 		nl = "* Je hebt ontslag genomen van je baan (%s)."
 	},
 	JOB_NOT_EMPLOYED = {
 		en_US = "* You cannot resign as you are not currently employed.",
+		es = "* No pudes abandonar un empleo porque no eres empleado.",
 		nl = "* Je kunt geen ontslag nemen aangezien je momenteel geen werk hebt."
 	},
 	CHAT_REPEATING = {
 		en_US = "* Stop repeating yourself!",
+		es = "* Deja de repetirte!"
 		nl = "Stop met jezelf te herhalen!"
 	},
 	CHAT_TOO_FAST = {
 		en_US = "* You are chatting too fast!",
+		es = "* Estas chateando demasiado rapido!",
 		nl = "* Je bent te snel aan het chatten!"
 	},
 	CHAT_LOGIN = {
 		en_US = "* You must login to use chat!",
+		es = "* Debes estar logueado para usar el chat!",
 		nl = "* Je moet ingelogd zijn om de chat te kunnen gebruiken!"
 	},
 	PEAK_NEW = {
 		en_US = "* A new player peak has been reached! The new peak is %s players.",
+		es = "* Un nuevo record de jugadores concurrentes fue alcanzado! El nuevo record es %s jugadores.",
 		nl = "* Een nieuwe spelers piek is bereikt! De nieuwe piek is %s spelers."
 	},
 	PEAK_CURRENT = {
 		en_US = "* The current player peak is %s players.",
+		es = "* El record actual de jugadores concurrentes es %s jugadores.",
 		nl = "* De huidige spelers piek is %s spelers."
 	},
 	PEAK_ERROR = {
 		en_US = "* There was an error loading the player peak.",
+		es = "* Ha ocurrido un error cargando el record de jugadores concurrentes.",
 		nl = "* Er is een fout opgetreden tijdens het laden van de spelers piek."
 	},
 	PLAYTIME_REWARD = {
 		en_US = "* You have been paid $%s as a reward for playing on the server for at least 50 minutes.",
+		es = "* Has sido recompensado $%s como premio por haber estado en el servidor por al menos 50 minutos.",
 		nl = "* Je hebt %s betaald gekregen als een beloning voor het spelen op deze server voor minimaal 50 minuten."
 	},
 	FIREFIGHTER_VEHICLE_FIRE = {
 		en_US = "* A vehicle has exploded in %s, %s.",
+		es = "* Un vehículo ha explotado en %s, %s.",
 		nl = "* Er is een voertuig geëxplodeerd in %s, %s."
 	},
 	FIREFIGHTER_EXTINGUISH = {
-		en_US = "* You have been paid $100 for extinguishing the fire."
+		en_US = "* You have been paid $100 for extinguishing the fire.",
+		es = "* Obtuviste una recompensa de $100 por apagar el incendio."
 	}
 }
